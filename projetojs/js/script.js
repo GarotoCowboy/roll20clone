1077887c62867f531c049cc13945f8a7d1f22e25
Pedro Henrique Marques Rocha <34407863+GarotoCowboy@users.noreply.github.com>
2023-11-29 08:38:05 -0300

    Add files via upload

--- projetojs/js/script.js
+++ projetojs/js/script.js
@@ -21,57 +21,85 @@ document.addEventListener("DOMContentLoaded", function () {
         };
         xhr.send();
     }
+
+    var swiper = new Swiper(".inicio-slider", {
+      spaceBetween: 20,
+      pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+          loop: true,
+          grabCursor: true,
+          centeredSlides: true,
+          autoplay: {
+              delay: 500,
+              disableOnInteraction: false,
+          },
+      },
+      breakpoints: {
+          0: {
+              slidesPerView: 1,
+          },
+          768: {
+              slidesPerView: 1,
+          },
+          968: {
+              slidesPerView: 1,
+          },
+      },
+  });
 });
+    
 
 
-document.addEventListener("DOMContentLoaded", function () {
-    // Carregar o conteúdo do form login
-    var containerLogin = document.getElementsByClassName("containerLogin");
 
-    // Verificar se o contêiner do menu existe na página
-    if (containerLogin) {
-        // Carregar o conteúdo do menu.html no contêiner do menu
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", "form-login.html", true);
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState == 4 && xhr.status == 200) {
-                header.innerHTML = xhr.responseText;
+// document.addEventListener("DOMContentLoaded", function () {
+//     // Carregar o conteúdo do form login
+//     var containerLogin = document.getElementsByClassName("containerLogin");
 
-                // Destacar o item do menu correspondente à página atual
-                highlightCurrentPage();
-                setupMenuClickEvent();
-                abrirTelaLogin()
-            }
-        };
-        xhr.send();
-    }
-});
+//     // Verificar se o contêiner do menu existe na página
+//     if (containerLogin) {
+//         // Carregar o conteúdo do menu.html no contêiner do menu
+//         var xhr = new XMLHttpRequest();
+//         xhr.open("GET", "form-login.html", true);
+//         xhr.onreadystatechange = function () {
+//             if (xhr.readyState == 4 && xhr.status == 200) {
+//                 header.innerHTML = xhr.responseText;
 
-// function highlightCurrentPage() {
-//     // Sua implementação para destacar a página atual no menu
-//     // ...
-
-//      // Obtém a URL da página atual
-//      var currentPageUrl = window.location.href;
-
-//      // Obtém todos os links no menu
-//      var menuLinks = document.querySelectorAll('#header a');
- 
-//      // Itera sobre os links do menu
-//      menuLinks.forEach(function (link) {
-//          // Obtém a URL do link atual no menu
-//          var linkUrl = link.href;
- 
-//          // Verifica se a URL da página atual corresponde à URL do link no menu
-//          if (currentPageUrl === linkUrl) {
-//              // Aplica algum destaque ao link, por exemplo, adicionando uma classe CSS
-//              link.classList.add('active');
-
-//              highlightCurrentPage();
+//                 // Destacar o item do menu correspondente à página atual
+//                 highlightCurrentPage();
 //                 setupMenuClickEvent();
-//          }
-//      });
-// }
+//                 abrirTelaLogin()
+//             }
+//         };
+//         xhr.send();
+//     }
+// });
+
+function highlightCurrentPage() {
+    // Sua implementação para destacar a página atual no menu
+    // ...
+
+     // Obtém a URL da página atual
+     var currentPageUrl = window.location.href;
+
+     // Obtém todos os links no menu
+     var menuLinks = document.querySelectorAll('#header a');
+ 
+     // Itera sobre os links do menu
+     menuLinks.forEach(function (link) {
+         // Obtém a URL do link atual no menu
+         var linkUrl = link.href;
+ 
+         // Verifica se a URL da página atual corresponde à URL do link no menu
+         if (currentPageUrl === linkUrl) {
+             // Aplica algum destaque ao link, por exemplo, adicionando uma classe CSS
+             link.classList.add('active');
+
+             highlightCurrentPage();
+                setupMenuClickEvent();
+         }
+     });
+}
 
 
 function abrirTelaLogin(){
@@ -96,29 +124,28 @@ function setupMenuClickEvent() {
 
 
 
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 20,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      loop: true,
-      grabCursor:true,
-      centeredSlides:true,
-      autoplay: {
-        delay: 9500,
-        disableOnInteraction: false,
-      },
-    },
-    breakpoints: {
-      0: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      968: {
-        slidesPerView: 3,
-      },
-    },
-  });
+// var swiper = new Swiper(".inicio-slider", {
+//     spaceBetween: 20,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//       loop: true,
+//       grabCursor:true,
+//       centeredSlides:true,
+//       autoplay: {
+//         delay: 500,
+//         disableOnInteraction: false,
+//       },
+//     },
+//     breakpoints: {
+//       0: {
+//         slidesPerView: 1,
+//       },
+//       768: {
+//         slidesPerView: 1,
+//       },
+//       968: {
+//         slidesPerView: 1,
+//       },
+//     },
+//   });
